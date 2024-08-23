5f4eaba1a70482f90a80abec739436b05824b0a0
Guglielmettiii <saraguglielmetti0@gmail.com>
2024-08-23 18:55:45 +0200

    modifiche_start

--- SlideShow_JS.js
+++ SlideShow_JS.js
@@ -18,17 +18,27 @@ function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
+
+  // Gestione dell'indice fuori dai limiti
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
+
+  // Nascondere tutte le diapositive
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
+
+  // Rimuovere la classe "active" da tutti i punti
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].classList.remove("active");
   }
-  slides[slideIndex-1].style.display = "block";
-  if (dots.length > 0) { // Ensure that dots exist before trying to manipulate them
-    dots[slideIndex-1].className += " active";
+
+  // Mostrare la diapositiva corrente e attivare il punto corrispondente
+  slides[slideIndex - 1].style.display = "block";
+  if (dots.length > 0) { // Assicurarsi che ci siano i punti prima di manipolarli
+    dots[slideIndex - 1].classList.add("active");
   }
 }
+
+
 
